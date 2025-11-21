bb837536e7d2f97b304d03843397c8965cc3ee62
igoeldc <igoel5k@gmail.com>
2025-11-21 00:20:02 -0600

    Update __all__ in __init__.py for better organization

--- mathcode/__init__.py
+++ mathcode/__init__.py
@@ -1,2 +1,15 @@
-from . import optimization
-from . import stochastics
+from . import (
+    abstract_algebra,
+    graph_theory,
+    optimization,
+    reinforcement_learning,
+    stochastics,
+)
+
+__all__ = [
+    "abstract_algebra",
+    "graph_theory",
+    "optimization",
+    "reinforcement_learning",
+    "stochastics",
+]
